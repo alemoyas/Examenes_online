62501ed89f2457d99401a265706f3d0626fd8d14
alemoyas <sistemarestobar@hotmail.com>
2021-11-04 23:54:27 -0300

    asda

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usando php</title>
+    <style>
+        body{
+            display: flex;
+            justify-contect: center;
+            align-items: center;
+            flex-direction: column;
+            min-height: 100vh
+        }
+    </style>
 </head>
 <body>
     <form action="upload.php" method="post" enctype="multipart/form-data">
